329769ad76060844fa07e6af4afa6c6244277cf5
Boi <youngjay050186@gmail.com>
2022-07-23 23:20:37 +0800

    Updated derivatives

--- cppprojects/derivatives.h
+++ cppprojects/derivatives.h
@@ -13,7 +13,6 @@ class calculator;
 class rules {
 public:
 	void show() {
-		cout << "Constant: f'x c = 0 \n";
 		cout << "Power rule: f'x x^n = nx^n-1 \n";
 		cout << "Sine : f'x sin(x) = cos(x) \n";
 		cout << "Cosine : f'x cos(x) = -sin(x) \n";
@@ -21,8 +20,7 @@ public:
 		cout << "Natural Logarithm : f'x Ln(x) = 1/x \n";
 		cout << "Tangent : f'x tan(x) = sec^2(x) \n";
 		cout << "Logarithm : f'x loga(x) = 1/x * Ln(a) \n";
-		cout << "ax : f'x ax = a \n";
-		cout << "Line : f'x x = 1 \n";
+		cout << "Line : f'x 5x = 5 \n";
 		cout << "f'x lim T>0 f(x+Tx) - f(x) / Tx \n";
 		cout << "Square root : f'x sqrt(x) = (1/x)*x^-1/2 \n";
 	}
@@ -34,11 +32,7 @@ public:
 		string input;
 		cout << "Enter the derivative functions (Lists can be found in the command helpderivative: \n";
 		cin >> input;
-		if (input == "Constant")
-		{
-			
-		}
-		else if (input == "Power rule")
+		if (input == "Power rule")
 		{
 			double power, number, top, bottom, ans;
 			cout << "Enter the amount of x : \n";
@@ -68,7 +62,15 @@ public:
 		}
 		else if (input == "Exponential")
 		{
-
+			double number, x,left, right, ans;
+			cout << "Enter the number of x : \n";
+			cin >> x;
+			cout << "Enter the number of the constant : \n";
+			cin >> number;
+			left = pow(number, x);
+			right = log(number);
+			ans = left * right;
+			cout << "The answer is : " << ans << "\n";
 		}
 		else if (input == "Natural Logarithm")
 		{
@@ -80,23 +82,42 @@ public:
 		}
 		else if (input == "Tangent")
 		{
-
+			double number, ans, secant;
+			cout << "Enter the number of x : \n";
+			cin >> number;
+			secant = 1 / cos(number);
+			ans = pow(secant, 2);
+			cout << "The answer is : " << ans << "\n";
 		}
 		else if (input == "Logarithm")
 		{
-
-		}
-		else if (input == "ax")
-		{
-
+			double number, base, left, right, ans;
+			cout << "Enter the number of x : \n";
+			cin >> number;
+			cout << "Enter the base of the logarithm : \n";
+			cin >> base;
+			left = 1 / number;
+			right = log(base);
+			ans = left * right;
+			cout << "The answer is : " << ans << "\n";
 		}
 		else if (input == "Line")
 		{
-
+			double number;
+			cout << "Enter the number beside x : \n";
+			cin >> number;
+			cout << "The answer is : " << number << "\n";
 		}
 		else if (input == "Square root")
 		{
-
+			//(1/x)*x^-1/2
+			double number, left, right, ans;
+			cout << "Enter the number of x : \n";
+			cin >> number;
+			left = 1 / number;
+			right = pow(number, -0.5);
+			ans = left * right;
+			cout << "The answer is : " << ans << "\n";
 		}
 		else
 		{
